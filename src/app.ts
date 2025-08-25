65d8c1c154be6f4aeb50de279b6af87cb8f320d3
SIRAWICH-SRI <SIRAWICH-SRI@rmutp.ac.th>
2025-08-25 08:45:38 +0700

    add app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-//operation
-//CRUD
-app.get("/", (c) => c.text("Hello World"));
+app.get("/", (c) => c.text("Hello World 🚀"));
 
 export default app;
